d4abb021cfe19dc2fd920e03b83ea02cc5bc786b
ShiningLight <aizy.fpg@gmail.com>
2020-10-16 23:54:15 +0300

    feat: add solution v2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
     do {
-        var strBefore = str;
-        for (const [left, right] of bracketsConfig) {
+        var strBeforeReplacement = str;
+        for (const [leftBracket, rightBracket] of bracketsConfig) {
             str = str.replace(`${left}${right}`, '');
         }
-    } while (str !== strBefore)
+    } while (str !== strBeforeReplacement)
 
     return !str;
 }
